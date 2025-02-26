a9c48b7a41a588f203248e3f273107c2609e5378
ety001 <work@akawa.ink>
2025-02-26 12:05:22 +0800

    update

--- filezilla/docker_config/custom-cont-init.d/init-filezilla-config.sh
+++ filezilla/docker_config/custom-cont-init.d/init-filezilla-config.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+find /etc/s6-overlay/s6-rc.d/ -name "run" -type f -print0 | while IFS= read -r -d '' file; do echo ${file}; sed -i 's/abc/root/g' "${file}"; done
+
 mkdir -p /config/.config/filezilla
 [ ! -f /config/.config/filezilla/filezilla.xml ] && cp /lzcapp/pkg/content/filezilla.xml /config/.config/filezilla/filezilla.xml
 sed -i "s/LZC_USER_NAME/${LAZYCAT_APP_DEPLOY_UID}/g" /config/.config/filezilla/filezilla.xml
