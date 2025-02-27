7b5886cc0e1b5be9e27f7cac481433d944537d67
ety001 <work@akawa.ink>
2025-02-27 15:45:05 +0800

    update

--- kdenlive/docker_config/custom-cont-init.d/init-config.sh
+++ kdenlive/docker_config/custom-cont-init.d/init-config.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 [ ! -f /config/.config/kdenliverc ] && cp /lzcapp/pkg/content/kdenliverc /config/.config/kdenliverc
 sed -i "s/LZC_USER_NAME/${LAZYCAT_APP_DEPLOY_UID}/g" /config/.config/kdenliverc
+[ ! -f /config/.config/QtProject.conf ] && cp /lzcapp/pkg/content/QtProject.conf /config/.config/QtProject.conf
+sed -i "s/LZC_USER_NAME/${LAZYCAT_APP_DEPLOY_UID}/g" /config/.config/QtProject.conf
