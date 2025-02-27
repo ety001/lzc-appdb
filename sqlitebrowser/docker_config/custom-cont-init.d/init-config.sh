5c1efe003835b38ca03a4f1186db4a90880adc7f
ety001 <work@akawa.ink>
2025-02-27 18:10:30 +0800

    update

--- sqlitebrowser/docker_config/custom-cont-init.d/init-config.sh
+++ sqlitebrowser/docker_config/custom-cont-init.d/init-config.sh
@@ -2,3 +2,5 @@
 mkdir -p /config/.config/sqlitebrowser
 [ ! -f /config/.config/sqlitebrowser/sqlitebrowser.conf ] && cp /lzcapp/pkg/content/sqlitebrowser.conf /config/.config/sqlitebrowser/sqlitebrowser.conf
 sed -i "s/LZC_USER_NAME/${LAZYCAT_APP_DEPLOY_UID}/g" /config/.config/sqlitebrowser/sqlitebrowser.conf
+[ ! -f /config/.config/QtProject.conf ] && cp /lzcapp/pkg/content/QtProject.conf /config/.config/QtProject.conf
+sed -i "s/LZC_USER_NAME/${LAZYCAT_APP_DEPLOY_UID}/g" /config/.config/QtProject.conf
