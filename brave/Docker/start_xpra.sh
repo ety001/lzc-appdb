ba3a200b9b2619b50c0651154ede1a4c7faa23b1
ety001 <work@akawa.ink>
2025-06-26 14:19:58 +0800

    update

---
+++ brave/Docker/start_xpra.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+program="/usr/bin/brave-browser --no-sandbox"
+
+xpra start :100 \
+  --daemon=no \
+  --opengl=yes \
+  --encoding=h264 \
+  --video-encoders=vaapi \
+  --compressors=lz4 \
+  --dpi=96 \
+  --quality=20 \
+  --speed=30 \
+  --html=on \
+  --bind-tcp=0.0.0.0:10000 \
+  --webcam=no \
+  --start="${program}"
