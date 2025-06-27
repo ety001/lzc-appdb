2af8c732f18d60502d4452f8108b2a6a2078ec5d
ety001 <work@akawa.ink>
2025-06-27 19:24:30 +0800

    obsidian

---
+++ obsidian/Docker/start_xpra copy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+export LIBVA_DRIVER_NAME=iHD
+export LIBVA_DRIVERS_PATH=/usr/lib/x86_64-linux-gnu/dri/
+export XDG_RUNTIME_DIR=/run/user/1001
+export IBUS_ENABLE_SYNC_MODE=1
+export GTK_IM_MODULE=ibus
+export XMODIFIERS=@im=ibus
+export QT_IM_MODULE=ibus
+export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1001/bus
+
+main_program="/usr/bin/obsidian"
+
+exec xpra start :100 \
+  --daemon=no \
+  --opengl=yes \
+  --encoding=h264 \
+  --video-encoders=vaapi \
+  --dpi=96 \
+  --quality=40 \
+  --speed=50 \
+  --html=on \
+  --bind-tcp=0.0.0.0:10000 \
+  --webcam=no \
+  --start="${main_program}"
