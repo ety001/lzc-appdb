f3df78b82a5ec2ce1278f99fcf84c79efa18dbf1
ety001 <work@akawa.ink>
2025-07-01 11:12:23 +0800

    update

---
+++ zettlr/Docker/start_xpra.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+export LIBVA_DRIVER_NAME=iHD
+export LIBVA_DRIVERS_PATH=/usr/lib/x86_64-linux-gnu/dri/
+export XDG_RUNTIME_DIR=/run/user/1001
+export IBUS_ENABLE_SYNC_MODE=1
+export GTK_IM_MODULE=ibus
+export XMODIFIERS=@im=ibus
+export QT_IM_MODULE=ibus
+export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1001/bus
+
+exec xpra start :0 \
+  --daemon=no \
+  --opengl=yes \
+  --encoding=h264 \
+  --video-encoders=vaapi \
+  --dpi=120 \
+  --xvfb="Xvfb -screen 0 1920x1080x24" \
+  --quality=50 \
+  --speed=50 \
+  --html=on \
+  --bind-tcp=0.0.0.0:10000 \
+  --webcam=no
