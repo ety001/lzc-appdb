3abcd3c55dfd08a1aa0a44e44bd28ad029a2be0a
ety001 <work@akawa.ink>
2025-11-10 00:36:29 +0800

    add proxy

--- brave/Docker/build.sh
+++ brave/Docker/build.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-docker build --push -t ety001/brave-browser:ubuntu-noble -f Dockerfile .
+docker build --push \
+    --build-arg HTTPS_PROXY=http://192.168.199.11:8001 \
+    -t ety001/brave-browser:ubuntu-noble \
+    -f Dockerfile .
